2d51d0463743ce09ad29e6370bb4f19a4d732fd7
Pio <plollaton@hotmail.com>
2019-12-13 17:29:46 -0300

    aula 3 item 4 concluido.

--- mod2/src/app/rotas/rotas.js
+++ mod2/src/app/rotas/rotas.js
@@ -1,3 +1,6 @@
+const LivroDao = require("../infra/livro-dao");
+const db = require("../../config/database");
+
 module.exports = app => {
   app.get("/", (req, resp) => {
     resp.marko(require("../views/livros/lista/lista.marko"), {
@@ -11,6 +14,14 @@ module.exports = app => {
           titulo: "Node Avançado"
         }
       ]
+    });
+  });
+  app.get("/livros", (req, resp) => {
+    const livroDao = new LivroDao(db);
+    livroDao.lista((erro, resultados) => {
+      resp.marko(require("../views/livros/lista/lista.marko"), {
+        livros: resultados
+      });
     });
   });
 };
