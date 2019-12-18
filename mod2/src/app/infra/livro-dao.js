03b75f65ebba2c0a1c72e3841906dfa7162f5a46
Pio <plollaton@hotmail.com>
2019-12-18 15:20:48 -0300

    inicio parte 2 node JS - projeto atualizado como o da aula.

--- mod2/src/app/infra/livro-dao.js
+++ mod2/src/app/infra/livro-dao.js
@@ -1,80 +1,112 @@
 class LivroDao {
-  constructor(db) {
-    this._db = db;
-  }
 
-  lista() {
-    return new Promise((resolve, reject) => {
-      this._db.all("SELECT * FROM livros", (erro, resultados) => {
-        if (erro) return "Não foi possível listar os livros.";
-        return resolve(resultados);
-      });
-    });
-  }
+    constructor(db) {
+        this._db = db;
+    }
 
-  adiciona(livro) {
-    return new Promise((resolve, reject) => {
-      this._db.run(
-        `INSERT INTO livros(titulo, preco, descricao) VALUES (?,?,?)`,
-        [livro.titulo, livro.preco, livro.descricao],
-        err => {
-          if (err) {
-            console.log(err);
-            return reject("Não foi possível adicionar o livro");
-          }
-        }
-      );
-    });
-  }
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                    titulo, 
+                    preco,
+                    descricao
+                ) values (?,?,?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
 
-  buscaPorId(id) {
-    return new Promise((resolve, reject) => {
-      this._db.get("SELECT * FROM livros WHERE id = ?", [id], (err, row) => {
-        if (err) {
-          console.log(err);
-          return reject(`Um erro ocorreu ao realizar a consulta.`);
-        }
-        if (!row) {
-          console.log("Nao localizado");
-          return reject(`Não localizado nenhum livro com o id: ${id}.`);
-        }
-        console.log(row);
-        return resolve(row);
-      });
-    });
-  }
+                    resolve();
+                }
+            )
+        });
+    }
 
-  autaliza(livro) {
-    return new Promise((resolve, reject) => {
-      this._db.run(
-        `UPDATE livros 
-            SET titulo = ?,
+    lista() {
+        return new Promise((resolve, reject) => {
+            this._db.all(
+                'SELECT * FROM livros',
+                (erro, resultados) => {
+                    if (erro) return reject('Não foi possível listar os livros!');
+
+                    return resolve(resultados);
+                }
+            )
+        });
+    }
+
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET
+                titulo = ?,
                 preco = ?,
                 descricao = ?
-          WHERE id = ?`,
-        [livro.titulo, livro.preco, livro.descricao, livro.id],
-        err => {
-          if (err) {
-            console.log(err);
-            return reject(`Não foi possível atualizar o livro ${livro.id}.`);
-          }
-          return resolve();
-        }
-      );
-    });
-  }
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            erro => {
+                if (erro) {
+                    return reject('Não foi possível atualizar o livro!');
+                }
 
-  remove(id) {
-    return new Promise((resolve, reject) => {
-      this._db.run(`DELETE FROM livros WHERE id = ?`, [id], err => {
-        if (err) {
-          console.log(err);
-          return reject(`Não foi possível excluir o livro ${id}.`);
-        }
-        return resolve();
-      });
-    });
-  }
+                resolve();
+            });
+        });
+    }
+
+    remove(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE 
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
+        });
+    }
 }
 
 module.exports = LivroDao;
--- mod2/src/app/rotas/rotas.js
+++ mod2/src/app/rotas/rotas.js
@@ -1,80 +1,68 @@
-const LivroDao = require("../infra/livro-dao");
-const db = require("../../config/database");
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
 
-module.exports = app => {
-  app.get("/", (req, resp) => {
-    resp.send("Cadastro de livros");
-  });
+module.exports = (app) => {
+    app.get('/', function(req, resp) {
+        resp.marko(
+            require('../views/base/home/home.marko')
+        );
+    });
+    
+    app.get('/livros', function(req, resp) {
 
-  app.get("/livros", (req, resp) => {
-    const livroDao = new LivroDao(db);
-    livroDao
-      .lista()
-      .then(livros =>
-        resp.marko(require("../views/livros/lista/lista.marko"), {
-          livros: livros
-        })
-      )
-      .catch(erro => console.log(erro));
-  });
+        const livroDao = new LivroDao(db);
+        livroDao.lista()
+                .then(livros => resp.marko(
+                    require('../views/livros/lista/lista.marko'),
+                    {
+                        livros: livros
+                    }
+                ))
+                .catch(erro => console.log(erro));
+    });
 
-  app.get("/livros/form", (req, resp) => {
-    resp.marko(require("../views/livros/form/form.marko"), { livro: {} });
-  });
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
+    });
 
-  app.get("/livros/form/:id", (req, resp) => {
-    const id = req.params.id;
-    const livroDao = new LivroDao(db);
-    livroDao
-      .buscaPorId(id)
-      .then(livro =>
-        resp.marko(require("../views/livros/form/form.marko"), {
-          livro: livro
-        })
-      )
-      .catch(erro => console.log(erro));
-  });
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
 
-  app.get("/livros/livro/:livroId", (req, resp) => {
-    let livroId = req.params.livroId;
-    const livroDao = new LivroDao(db);
-    livroDao
-      .buscaPorId(livroId)
-      .then(livro =>
-        resp.marko(require("../views/livros/lista/livro.marko"), {
-          livro: livro
-        })
-      )
-      .catch(erro => {
-        console.log(erro);
-        resp.send(erro);
-      });
-  });
+        livroDao.buscaPorId(id)
+                .then(livro => 
+                    resp.marko(
+                        require('../views/livros/form/form.marko'), 
+                        { livro: livro }
+                    )
+                )
+                .catch(erro => console.log(erro));
+    });
 
-  app.post("/livros", (req, resp) => {
-    console.log(req.body);
-    const livroDao = new LivroDao(db);
-    livroDao
-      .adiciona(req.body)
-      .then(resp.redirect("/livros"))
-      .catch(erro => console.log(erro));
-  });
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
 
-  app.put("/livros", (req, resp) => {
-    console.log(req.body);
-    const livroDao = new LivroDao(db);
-    livroDao
-      .autaliza(req.body)
-      .then(resp.redirect("/livros"))
-      .catch(erro => console.log(erro));
-  });
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.atualiza(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
 
-  app.delete("/livros/:id", (req, resp) => {
-    const id = req.params.id;
-    const livroDao = new LivroDao(db);
-    livroDao
-      .remove(id)
-      .then(() => resp.status(200).end())
-      .catch(erro => console.log(erro));
-  });
+    app.delete('/livros/:id', function(req, resp) {
+        const id = req.params.id;
+
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+                .then(() => resp.status(200).end())
+                .catch(erro => console.log(erro));
+    });
 };
